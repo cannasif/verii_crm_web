5002b412cbcd132c67273273732906a08019349b
Can Nasıf <can.nasif@v3rii.com>
2026-08-04 01:24:46 +0300

    chore: kullanılmayan eski aktivite modüllerini kaldır

--- src/features/activity-shipping-management_old/index.ts
+++
@@ -1,2 +0,0 @@
-export { PaymentTypeManagementPage } from './components/PaymentTypeManagementPage';
-export type { PaymentTypeDto, CreatePaymentTypeDto, UpdatePaymentTypeDto } from './types/payment-type-types';
